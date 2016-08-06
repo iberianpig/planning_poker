305c0bee0966d0606b855d0ab79ae34b0b53ab34
areph <40balmung@gmail.com>
2016-08-06 19:02:35 +0900

    Temp

---
+++ app/assets/javascripts/components/score_board.js.jsx
@@ -0,0 +1,17 @@
+/*
+var ScoreBoard = React.createClass({
+  render: function() {
+    return <p>hello!</p>
+  }
+});
+*/
+
+var ScoreBoard = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
