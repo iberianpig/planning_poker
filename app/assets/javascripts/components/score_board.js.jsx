750cd09d68a215c0367cfc81c09e686069bd6e38
iberianpig <yhkyky@gmail.com>
2016-08-07 00:13:58 +0900

    add thead

--- app/assets/javascripts/components/score_board.js.jsx
+++ app/assets/javascripts/components/score_board.js.jsx
@@ -1,6 +1,8 @@
+var columns = ['task', 'user1', 'user2']
 var tasks = [
   {id: 1, task: "機能", user1: "Aさん", user2: "Bさん"},
   {id: 2, task: "テーブルを作る", user1: "2", user2: "3"},
+  {id: 3, task: "タスクを作る", user1: "1", user2: "4"},
 ];
 var TaskRow = React.createClass({
   render: function() {
@@ -11,16 +13,26 @@ var TaskRow = React.createClass({
 });
 var ScoreBoard = React.createClass({
   render: function(){
+    var taskHeaders = columns.map(function(column){
+        return (
+          <th>{column}</th>
+        );
+    });
     var taskNodes = tasks.map(function(row) {
       return (
         <TaskRow score={row} key={row.id} />
       );
     });
     return (
-      <table>
-      <tbody>
-      {taskNodes}
-      </tbody>
+      <table className='table table-striped table-hover'>
+        <thead>
+          <tr>
+            {taskHeaders}
+          </tr>
+        </thead>
+        <tbody>
+          {taskNodes}
+        </tbody>
       </table>
     );
   }
